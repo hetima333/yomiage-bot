fe38f366057d16604867d83ff27fb4a623e8f6c3
hetima333 <hetima333@gmail.com>
2020-09-18 18:25:08 +0900

    [fix]変換を逆にしてみる

--- cogs/utils/voice_util.py
+++ cogs/utils/voice_util.py
@@ -58,7 +58,8 @@ class VoiceFactory():
             sounds = json.loads(f.read())
 
         # 全角チルダを波ダッシュに置換
-        msg = msg.replace(u'ff5e', u'301c')
+        # msg = msg.replace(u'ff5e', u'301c')
+        msg = msg.replace(u'301c', u'ff5e')
 
         for v in sounds:
             r = re.fullmatch(f"{v['reg']}", msg, re.I)
