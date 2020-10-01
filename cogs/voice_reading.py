65db8b3c81055ac7134d48133c0b59cdb4b37886
hetima333 <hetima333@gmail.com>
2020-10-01 22:46:04 +0900

    [fix]コマンドまで読み上げていたのを修正

--- cogs/voice_reading.py
+++ cogs/voice_reading.py
@@ -389,9 +389,10 @@ class VoiceReading(commands.Cog, name='VC読み上げ'):
 
         vc = self.get_guild_voice_client(message.guild.id)
         if vc is not None:
-            msg = self._convert_message(message.clean_content)
+            msg = message.clean_content()
             if message.content.startswith('=sc'):
-                msg = f"{message.author.display_name}さんがスパチャしました。{msg}"
+                msg = f"{message.author.display_name}さんがスパチャしました。{msg[4:]}"
+            msg = self._convert_message(message.clean_content)
             vf = await VoiceFactory.create_voice(msg, message.author.id)
             if vf is None:
                 return
